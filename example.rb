b610185a9ddb38e5ce5bcfbbbb2ca030d2790c8a
Tom Taylor <tom@tommyt.co.uk>
2015-02-16 13:31:51 +0000

    Added some example code

---
+++ example.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'lib/haproxystats.rb'
+
+socket = '/var/run/haproxy.stats'
+
+# Create stats object and bind to socket
+ha = HAProxyStats.new(socket)
+
+# Tell stats object to slurp some stats
+ha.retrieve
+
+# Get a list of services
+my_services = ha.services
+puts "#{my_services.length} services found"
+
+# How many backends are up per service
+my_services.each do |service|
+    puts "#{service}: #{ha.up(service)} of #{ha.backends(service).length}"
+end
+
+# What ratio of servers are up per service
+my_services.each do |service|
+    puts "#{service}: #{ha.upratio(service)}"
+end
